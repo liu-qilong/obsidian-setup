aad86aab086d83a6c45d701e94234170ef75f7fa
KNPOB <qilong-kirov.liu@outlook.com>
2023-12-29 17:43:11 +0800

    rewrote bibtex parser

--- view/doi2bib/view.js
+++ view/doi2bib/view.js
@@ -16,36 +16,70 @@ dv.header(2, 'BibTeX to YAML')
 
 dv.paragraph('The YAML format entry information should be shown as follows.')
 
-function parseBibTeX(bibtexData) {
-	const entries = [];
+function parse_bitex(bibtexData) {
+
+	// match bibtype, bibid, & fields
 	const entryRegex = /@([a-zA-Z]+){([^,]+),(.*)}/g;
-	
 	let match = entryRegex.exec(bibtexData)
-	const bibtype = match[1]
-	const bibid = match[2]
-	const fields = match[3].split(',  ')
-	
-	const entry = {
-	  bibtype,
-	  bibid,
+
+	let fields = {}
+	fields['bibtype'] = match[1]
+	fields['bibid'] = match[2]
+	let fields_str = match[3]
+
+	// parse bibtex fields
+	let mode = 'key'
+	let store = ''
+	let max_layer = 0
+	let stack = []
+	let keys = []
+	let values = []
+	const head_trim = /^[,={} ]+/
+	const trail_trim = /[,={} ]+$/
+
+	for (let [idx, char] of [...fields_str].entries()) {
+		if (mode === 'key') {
+			// parsing the key of a field
+			if (char === '=') {
+				keys.push(store.replace(head_trim, '').replace(trail_trim, ''))
+				store = ''
+				mode = 'value'
+			} else {
+				store += char
+			}
+		} else if (mode === 'value') {
+			// parsing the value of a field
+			store += char
+
+			if (char === '{') {
+				stack.push(char)
+				max_layer += 1
+			} else if (char === '}') {
+				stack.pop()
+			}
+
+			if (max_layer > 0 && stack.length === 0) {
+				// when the field has {} layers, complete parsing when the { stack is empty
+				values.push(store.replace(head_trim, '').replace(trail_trim, '').replace(':', '{:}'))
+				store = ''
+				max_layer = 0
+				mode = 'key'
+			} else if (max_layer === 0 && (char === ',' || char === '}' || idx === fields_str.length - 1)) {
+				// when the field has no {} layers, complete parsing when the ,/} is encountered or the string ends
+				values.push(store.replace(head_trim, '').replace(trail_trim, '').replace(':', '{:}'))
+				store = ''
+				mode = 'key'
+			}
+		}
 	}
 	
-	fields.forEach((field) => {
-		const [name, value] = field.split('=')
-		
-		if (value.trim()[0] == '{') {
-			entry[name.trim()] = value.trim().slice(1, -1)
-		} else {
-			entry[name.trim()] = value.trim()
-		}
-	})
-	
-	return entry
+	keys.map((key, idx) => { fields[key] = values[idx] })
+	return fields
 }
 
 if (file.bibtex != null) {
 	const bibtex = file.bibtex
-	const bibjson = parseBibTeX(bibtex)
+	const bibjson = parse_bitex(bibtex)
 	let commands = ['```']
 
 	for (let [key, value] of Object.entries(bibjson)) {
