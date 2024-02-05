f96bab5f1d0b3370535111577e4e60c154decaba
KNPOB <qilong-kirov.liu@outlook.com>
2024-02-05 10:53:12 +0800

    tag based paper&thread pages

--- view/paper/view.js
+++ view/paper/view.js
@@ -1,12 +1,13 @@
-const file = dv.current()
+const current_file = dv.current()
+const current_name = dv.current().file.name
 
 // bibtex
 dv.header(2, 'BibTex')
 
-let commands = ['```', `@${file.bib_type}{${file.bib_id},`]
+let commands = ['```', `@${current_file.bib_type}{${current_file.bib_id},`]
 let flag = false
 
-for (let [key, value] of Object.entries(file)) {
+for (let [key, value] of Object.entries(current_file)) {
 	if (key == 'bib_id') {
 		flag = true
 		continue
@@ -21,81 +22,128 @@ commands.push('}', '```')
 
 dv.paragraph(commands.join('\n'))
 
-// flow
-dv.header(2, 'Flow')
+// thread view
+dv.header(2, 'Thread')
 
-mermaid_style = "%%{ init: { 'themeVariables': { 'nodeBorder': '#00000000', 'mainBkg': '#00000000' }}}%%"
+const mermaid_style = "%%{ init: { 'themeVariables': { 'nodeBorder': '#00000000', 'mainBkg': '#00000000' }}}%%"
 // nodeBorder: class box segment line color
 // mainBkg: background color of the links' text box
 
 commands = [`\`\`\`mermaid\n${mermaid_style}\nclassDiagram`]
 
-let badge2emoji = {
-    'skimmed': '🪫',
-    'read': '🔋',
-    'seminal': '💡',
-	'important': '📌',
-	'work-well': '👍',
-}
+function paper_node(p, commands) {
+	let badge2emoji = {
+		'skimmed': '🪫',
+		'read': '🔋',
+		'seminal': '💡',
+		'important': '📌',
+		'work-well': '👍',
+	}
 
-function paper_node(p) {
 	let badge_str = (dv.isArray(p.bib_badge) && p.bib_badge.length > 0)?(p.bib_badge.map(p => badge2emoji[p]).join('')):('')
 	let note_str = (dv.isArray(p.bib_note) && p.bib_note.length > 0)?(p.bib_note.join('\n')):('')
 	let comment_str = (dv.isArray(p.bib_remark) && p.bib_remark.length > 0)?(p.bib_remark.map(p => `*(${p})`).join('\n')):('')
 
 	if (badge_str + note_str + comment_str != '') {
-		return `class ${p.bib_id} {\n${badge_str} ${note_str}\n${comment_str}}`
+		commands.push(`class ${p.bib_id} {\n${badge_str} ${note_str}\n${comment_str}}`)
 	} else {
-		return `class ${p.bib_id}`
+		commands.push(`class ${p.bib_id}`)
 	}
 }
 
-p = dv.page(file.file.link)
-commands.push(paper_node(p))
+// draw current paper
+paper_node(current_file, commands)
 
-if (dv.isArray(file.bib_link)) {
-	// find linked papers
+// draw linked papers
+if (dv.isArray(current_file.bib_link)) {
 	let paper_links = []
-	paper_links.push(...file.bib_link)
+	paper_links.push(...current_file.bib_link)
 
-	// for (let p of dv.pages("#Paper and [[]]")) {
-	// 	for (let l of p.bib_link) {
-	// 		l.path = p.file.path
-	// 		paper_links.push(l)
-	// 	}
-	// }
-
-	// draw linked papers
 	for (let l of paper_links) {
-		commands.push(paper_node(dv.page(l)))
+		paper_node(dv.page(l), commands)
 
 		if (l.subpath != null) {
-			commands.push(`${file.bib_id} .. ${dv.page(l).bib_id}: ${l.subpath}`)
+			commands.push(`${current_file.bib_id} .. ${dv.page(l).bib_id}: ${l.subpath}`)
 		} else {
-			commands.push(`${file.bib_id} .. ${dv.page(l).bib_id}`)
+			commands.push(`${current_file.bib_id} .. ${dv.page(l).bib_id}`)
 		}
 	}
 }
 
-if (dv.isArray(file.bib_list)) {
-	// draw linked lists
-	function list_node(link, list_id) {
-		list = dv.page(link)
-		return `class List${list_id} {\n ${list.file.name}\n}`
+// parse nested threads
+function setup_nested_dict(dict, index) {
+	let current = dict
+	
+	for (let id of index) {
+		if (current[id] === undefined) {
+			current[id] = {}
+		}
+        current = current[id]
+	}
+}
+
+let threads = {}
+
+for (let tag of current_file.tags) {
+	if (tag.split('/')[0] === 'Thread') {
+		setup_nested_dict(threads, tag.split('/'))
+	}
+}
+
+// draw nested threads
+let thread_id_dict = {}
+let thread_ls = dv.pages('#Type/Thread')
+
+function thread_node(tag, source_tag, commands) {
+	let thread_title = `#${tag}`
+	let thread_id = `List-${Object.keys(thread_id_dict).length + 1}`
+	thread_id_dict[tag] = thread_id
+
+	for (let thread of thread_ls) {
+		if (thread.file.aliases[0] === `#${tag}`) {
+			thread_title = thread.file.name
+			break
+		}
 	}
 
-	let id = 1
+	if (source_tag != '') {
+		commands.push(`class ${thread_id} {\n${thread_title}\n}`)
+		let branch_tag = tag.replace(`${source_tag}/`, '')
 
-	for (let l of file.bib_list) {
-		commands.push(list_node(l, id))
-
-		if (l.subpath != null) {
-			commands.push(`List${id} -- ${file.bib_id}: ${l.subpath}`)
+		if (branch_tag.startsWith('pre-')) {
+			commands.push(`${thread_id} -- ${thread_id_dict[source_tag]}: ${branch_tag}`)
 		} else {
-			commands.push(`List${id} -- ${file.bib_id}`)
+			commands.push(`${thread_id_dict[source_tag]} -- ${thread_id}: ${branch_tag}`)
 		}
+	} else {
+		commands.push(`class ${thread_id} {\n${thread_title}\n#${tag}\n}`)
+	}
+}
 
-		id = id + 1
+function draw_threads(tag, node, source_tag) {
+	if (Object.keys(node).length === 1) {
+		// the current node only has 1 child
+		let sub_tag = Object.keys(node)[0]
+		let sub_node = Object.values(node)[0]
+		let pre_str = (tag != '')?(`${tag}/`):('')
+		draw_threads(`${pre_str}${sub_tag}`, sub_node, source_tag)
+	} else {
+		// the current node is a leaf node or have multiple children
+		thread_node(tag, source_tag, commands)
+
+		for (let [sub_tag, sub_node] of Object.entries(node)) {
+			pre_str = (tag != '')?(`${tag}/`):('')
+			draw_threads(`${pre_str}${sub_tag}`, sub_node, tag)
+		}
+	}
+}
+
+draw_threads('', threads, '')
+
+// draw links to threads
+for (let tag of current_file.tags) {
+	if (tag.split('/')[0] === 'Thread') {
+		commands.push(`${thread_id_dict[tag]} .. ${current_name}`)
 	}
 }
 
--- view/thread/view.js
+++ view/thread/view.js
@@ -1,138 +1,143 @@
+const current_file = dv.current()
+const current_tag = current_file.aliases[0].replace('#', '')
+const current_name = current_file.file.name
+
+let papers = dv.pages(`#Type/Paper and #${current_tag}`).sort(p => p.bib_year, 'asc')
+
 // flow view
-dv.header(2, 'Flow')
+dv.header(2, 'Thread')
 
-const file = dv.current()
-
-mermaid_style = "%%{ init: { 'themeVariables': { 'nodeBorder': '#00000000', 'mainBkg': '#00000000' }}}%%"
+const mermaid_style = "%%{ init: { 'themeVariables': { 'nodeBorder': '#00000000', 'mainBkg': '#00000000' }}}%%"
 // nodeBorder: class box segment line color
 // mainBkg: background color of the links' text box
 
-let commands = [`\`\`\`mermaid\n${mermaid_style}\nclassDiagram`]
-
-// root class
-commands.push(`class root {\n${file.file.name}\n}`)
-
-// paper class
-let papers = dv.pages(`#Type/Paper and [[]]`)
-	.sort(p => p.bib_year, 'asc')
-
-let badge2emoji = {
-    'skimmed': '🪫',
-    'read': '🔋',
-    'seminal': '💡',
-    'important': '📌',
-    'work-well': '👍',
-}
-
-function paper_node(p) {
-	let badge_str = (dv.isArray(p.bib_badge) && p.bib_badge.length > 0)?(p.bib_badge.map(p => badge2emoji[p]).join('')):('')
-	let note_str = (dv.isArray(p.bib_note) && p.bib_note.length > 0)?(p.bib_note.join('\n')):('')
-	let comment_str = (dv.isArray(p.bib_remark) && p.bib_remark.length > 0)?(p.bib_remark.map(p => `*(${p})`).join('\n')):('')
-
-	if (badge_str + note_str + comment_str != '') {
-		return `class ${p.bib_id} {\n${badge_str} ${note_str}\n${comment_str}}`
-	} else {
-		return `class ${p.bib_id}`
-	}
-}
-
-for (let p of papers) {
-	commands.push(paper_node(p))
-}
+let commands = []
+commands.push(`\`\`\`mermaid\n${mermaid_style}\nclassDiagram`)
+commands.push(`class root {\n${current_name}\n#${current_tag}\n}`)
 
 // parse paper branches
-let branches = { papers: [] }
+function push_nested_dict(dict, index, value) {
+	let current = dict
+	
+	for (let id of index) {
+		if (current[id] === undefined) {
+			current[id] = { __items__: [] }
+		}
+        current = current[id]
+	}
+
+	current.__items__.push(value)
+}
+
+let branches = { __items__: [] }
 
 for (let p of papers) {
-	for (let link of p.bib_list) {
-		if (link.path == file.file.path) {
-			if (link.subpath == null) {
-				branches.papers.push(p)
-			} else {
-                const idx = link.subpath.split('-')
-                let current_node = branches
-
-                for (let id of idx) {
-                    if (current_node[id] == null) {
-                        current_node[id] = { 'papers': [] }
-                    }
-                    current_node = current_node[id]
-                }
-                
-                current_node.papers.push(p)
-			}
+	for (let tag of p.tags) {
+		if (tag.includes(current_tag)) {
+            if (tag.includes(`${current_tag}/`)) {
+                // belongs to a sub tag
+                let branch_index = tag.replace(`${current_tag}/`, '').split('/')
+                push_nested_dict(branches, branch_index, p)
+            } else {
+                // belongs to the current tag
+                branches.__items__.push(p)
+            }
 		}
 	}
 }
 
 // draw branches
-function draw(name, node, start, mode) {
+let id_dict = []
+
+function draw_paper(p, id_dict, commands) {
+    let badge2emoji = {
+        'skimmed': '🪫',
+        'read': '🔋',
+        'seminal': '💡',
+        'important': '📌',
+        'work-well': '👍',
+    }
+    
+	let badge_str = (dv.isArray(p.bib_badge) && p.bib_badge.length > 0)?(p.bib_badge.map(p => badge2emoji[p]).join('')):('')
+	let note_str = (dv.isArray(p.bib_note) && p.bib_note.length > 0)?(p.bib_note.join('\n')):('')
+	let comment_str = (dv.isArray(p.bib_remark) && p.bib_remark.length > 0)?(p.bib_remark.map(p => `*(${p})`).join('\n')):('')
+
+    // set class name according to how many times an item appears in the thread
+    let bib_id = ''
+
+    if (id_dict[p.bib_id] === undefined) {
+        id_dict[p.bib_id] = 1
+        bib_id = p.bib_id
+    } else {
+        id_dict[p.bib_id] += 1
+        bib_id = `${p.bib_id}-${id_dict[p.bib_id]}`
+    }
+
+    // add class definition to mermaid commands
+	if (badge_str + note_str + comment_str != '') {
+		commands.push(`class ${bib_id} {\n${badge_str} ${note_str}\n${comment_str}}`) 
+	} else {
+		commands.push(`class ${bib_id}`)
+	}
+    
+    return bib_id
+}
+
+function draw_branch(name, node, start, mode) {
 	for (let [sub_name, sub_node] of Object.entries(node)) {
         let current = start
 
-        if (sub_name == 'papers') {
-            if (mode == 'sub') {
+        if (sub_name == '__items__') {
+            if (mode == 'downward') {
+                // draw downward branch items
                 for (let p of sub_node) {
+                    bib_id = draw_paper(p, id_dict, commands)
                     name_str = (name.length == 0)?(''):(`: ${name}`)
-                    commands.push(`${current} -- ${p.bib_id}${name_str}`)
-                    current = p.bib_id
+                    commands.push(`${current} -- ${bib_id}${name_str}`)
+                    current = bib_id
                 }
-            } else if (mode == 'pre') {
-                for (let p of sub_node) {
-                    name_str = (name.length == 0)?(''):(`: ${name}`)
-                    commands.push(`${p.bib_id} -- ${current}${name_str}`)
-                    current = p.bib_id
-                }
-            } else {
+            } else if (mode == 'upward') {
+                // draw upward branch items
                 for (let p of sub_node.toReversed()) {
-                    name_str = (name.length == 0)?(''):(`: ${name}`)
-                    commands.push(`${p.bib_id} .. ${current}${name_str}`)
-                    current = p.bib_id
+                    let bib_id = draw_paper(p, id_dict, commands)
+                    let name_str = (name.length == 0)?(''):(`: ${name}`)
+                    commands.push(`${bib_id} -- ${current}${name_str}`)
+                    current = bib_id
                 }
             }
         } else {
-            if (sub_node.papers.length > 0 & node.papers.length > 0) {
-                const sub_first = sub_node.papers[0]
+            if (node.__items__.length > 0 & sub_node.__items__.length > 0) {
+                // if current branch and sub branch both have items
+                // adjust the current drawing item to the last item in current branch
+                // that are published before the first item in sub branch
+                // if none, then set as the first item in current branch
+                current = node.__items__[0].bib_id
+                const sub_first = sub_node.__items__[0]
 
-                for (let p of node.papers) {
-                    if (sub_first.year > p.year) {
+                for (let p of node.__items__) {
+                    if (sub_first.bib_year > p.bib_year) {
                         current = p.bib_id
                     }
                 }
             }
-            if (sub_name == 'pre') {
-                draw(sub_name, sub_node, current, 'pre')
-            } else if (sub_name == 'link') {
-                draw(sub_name, sub_node, current, 'link')
-            } else {
-                draw(sub_name, sub_node, current, mode)
+            else if (node.__items__.length === 0) {
+                // if current branch doesn't have items
+                // combine current branch name to sub branch name
+                sub_name = (name != '')?(`${name}/${sub_name}`):(sub_name)
             }
             
+            if (sub_name.startsWith('pre-')) {
+                // if sub branch name contains 'pre-', set drawing mode as upward
+                draw_branch(sub_name, sub_node, current, 'upward')
+            } else {
+                // otherwise, pass current mode to the next level
+                draw_branch(sub_name, sub_node, current, mode)
+            }
         }
 	}
 }
 
-draw('', branches, 'root', 'sub')
-
-// paper links
-// const paper_ids = papers.bib_id
-
-// for (let p of papers) {
-//     if (dv.isArray(p.bib_link)) {
-//         for (let l of p.bib_link) {
-//             let lid = dv.page(l).bib_id
-
-//             if (paper_ids.includes(lid)) {
-//                 if (l.subpath != null) {
-//                     commands.push(`${p.bib_id} .. ${lid}: ${l.subpath}`)
-//                 } else {
-//                     commands.push(`${p.bib_id} .. ${lid}`)
-//                 }
-//             }
-//         }
-//     }
-// }
-
+draw_branch('', branches, 'root', 'downward')
 dv.paragraph(commands.join('\n'))
 
 // paper list
