8b76ba36d9a358acf12665325f6842149fd28c68
KNPOB <qilong-kirov.liu@outlook.com>
2024-03-07 12:41:03 +0800

    full automate doi2bib through api

--- bib2doi.md
+++ bib2doi.md
@@ -1,6 +1,5 @@
 ---
-doi: 
-bibtex: "@inproceedings{Cao2023,  title = {Self-Supervised Learning for Multimodal Non-Rigid 3D Shape Matching},  url = {http://dx.doi.org/10.1109/CVPR52729.2023.01701},  DOI = {10.1109/cvpr52729.2023.01701},  booktitle = {2023 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)},  publisher = {IEEE},  author = {Cao,  Dongliang and Bernard,  Florian},  year = {2023},  month = jun }"
+doi: 10.1007/978-3-319-24574-4_28
 ---
 
 ```dataviewjs
--- view/doi2bib/view.js
+++ view/doi2bib/view.js
@@ -1,15 +1,21 @@
 const file = dv.current()
 
-// doi2bib website
-dv.header(2, 'DOI to BibTeX')
+// get bibtex from doi
+async function get_bibtex(doi) {
+    const url = `https://doi.org/${doi}`
 
-dv.paragraph('Enter the `doi` to the note property pane and then copy the retrived BibTeX to the `bibtex` property:')
-
-if (file.doi != null) {
-	dv.paragraph(`<iframe src="https://www.doi2bib.org/bib/${file.doi}" width=100% height=300/>`)
-} else {
-	dv.paragraph('```\nplease fill the doi field\n```\n')
+    return fetch(url, {
+        headers: {
+            Accept: "application/x-bibtex",
+        },
+    })
+    .then(response => response.text())
+    .catch(error => {
+        console.error('Error:', error)
+    })
 }
+
+const bibtex = await get_bibtex(file.doi)
 
 // convert bibtex to yaml
 dv.header(2, 'BibTeX to YAML')
@@ -119,23 +125,18 @@ function parse_bitex(bibtexData, gen_id = false, lower_case_type = true) {
 	return fields
 }
 
-if (file.bibtex != null) {
-	const bibtex = file.bibtex
-	const bibjson = parse_bitex(bibtex, gen_id = true)
+const bibjson = parse_bitex(bibtex, gen_id = true)
 
-	// generated bib_id
-	dv.paragraph(`Generated entry ID:\n\`\`\`\n${bibjson['id']}\n\`\`\``)
+// generated bib_id
+dv.paragraph(`Generated entry ID:\n\`\`\`\n${bibjson['id']}\n\`\`\``)
 
-	// yaml
-	dv.paragraph('The YAML format entry information:')
-	let commands = ['```']
+// yaml
+dv.paragraph('The YAML format entry information:')
+let commands = ['```']
 
-	for (let [key, value] of Object.entries(bibjson)) {
-		commands.push(`bib_${key}: ${value}`)
-	}
-
-	commands.push('```')
-	dv.paragraph(commands.join('\n'))
-} else {
-	dv.paragraph('```\nplease fill the bibtex field\n```\n')
+for (let [key, value] of Object.entries(bibjson)) {
+	commands.push(`bib_${key}: ${value}`)
 }
+
+commands.push('```')
+dv.paragraph(commands.join('\n'))
